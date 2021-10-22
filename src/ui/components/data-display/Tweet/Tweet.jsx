0e15580fab19e0c12f7a79356d0a5d96156d2254
hanashiro <felipeaah@gmail.com>
2021-10-22 10:02:16 -0300

    projeto-07-03

---
+++ src/ui/components/data-display/Tweet/Tweet.jsx
@@ -0,0 +1,22 @@
+import styles from './Tweet.module.css';
+
+export default function Tweet({ tweet }) {
+    const { user } = tweet;
+    return (
+        <div className={styles['tweet-container']}>
+            <img
+                src={user.picture}
+                alt={user.name}
+                className={styles['avatar']}
+            />
+            <div className={styles['user']}>
+                <span className={styles['user-name']}>{user.name}</span>{' '}
+                <span className={styles['user-username']}>
+                    @{user.username}
+                </span>
+                <span className={styles['date']}> - {tweet.date}</span>
+            </div>
+            <div className={styles['tweet-text']}>{tweet.text}</div>
+        </div>
+    );
+}
--- src/ui/pages/index.jsx
+++ src/ui/pages/index.jsx
@@ -1,12 +1,24 @@
 import { useIndex } from '../../data/hooks/pages/useIndex.page';
 import styles from '../styles/pages/index.module.css';
 import TextInput from '../components/inputs/TextInput/TextInput';
+import Tweet from '../components/data-display/Tweet/Tweet';
+
+const tweet = {
+    date: 'há 5 dias',
+    text: 'Meu primeiro tweet',
+    user: {
+        name: 'Akira Hanashiro',
+        username: 'akirahanashiro',
+        picture: 'https://github.com/hanashiro.png',
+    },
+};
 
 export default function Index() {
     useIndex();
     return (
         <div>
             <TextInput />
+            <Tweet tweet={tweet} />
         </div>
     );
 }
